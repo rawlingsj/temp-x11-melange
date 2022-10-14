d31e3a564bcd3adfcccdcfbd0fbee3d047285715
James Rawlings <jrawlings@chainguard.dev>
2022-10-14 10:25:57 +0100

    more JDK deps

--- build.sh
+++ build.sh
@@ -5,11 +5,14 @@ FROM=${1:-000}
 FILES="*.yaml"
 for f in $FILES
 do
-  echo "Processing $f file..."
-  # take action on each file. $f store current file name
-  ORDER=$(echo $f | cut -d'-' -f 1)
-  echo checking $ORDER
-  if (( ${ORDER#0} >= $FROM )); then
-    docker run --platform linux/arm64 --privileged -v "$PWD":/work cgr.dev/chainguard/melange build /work/$f --arch amd64 --keyring-append local-melange.rsa.pub --signing-key local-melange.rsa
+  if [[ "$f" == *".yaml" ]]; then
+    echo "Processing $f file..."
+    # take action on each file. $f store current file name
+    ORDER=$(echo $f | cut -d'-' -f 1)
+    echo checking $ORDER
+    if (( ${ORDER#0} >= $FROM )); then
+        docker run --platform linux/arm64 --privileged -v "$PWD":/work cgr.dev/chainguard/melange build /work/$f --arch amd64 --keyring-append local-melange.rsa.pub --signing-key local-melange.rsa
+    fi
   fi
+
 done
