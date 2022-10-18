0fbea3960aa76a3d294d942de20799b9344224c2
James Rawlings <jrawlings@chainguard.dev>
2022-10-18 20:18:36 +0100

    working set of JDK melange conlfigs
    Signed-off-by: James Rawlings <jrawlings@chainguard.dev>

---
+++ build-gcb.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+# set -x
+
+FROM=${1:-000}    
+FILES=`ls | sort -n -t - -k 1`
+echo here $FILES
+
+for f in $FILES
+do
+  if [[ "$f" == "*.yaml" ]]; then
+    echo "Processing $f file..."
+    # take action on each file. $f store current file name
+    ORDER=$(echo $f | cut -d'-' -f 1)
+    echo checking $ORDER
+    #if (( "${ORDER#0}" >= "$FROM" )); then
+    if [ $ORDER -ge $FROM ]; then
+        echo would process $f
+        melange build $f --arch amd64 --keyring-append /secrets/local-melange.rsa.pub --signing-key /secrets/local-melange.rsa --out-dir /work/packages
+    fi
+  fi
+
+done
